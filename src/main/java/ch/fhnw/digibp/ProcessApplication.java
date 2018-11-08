ee64fa74feee6a343ac3b8a7e359496afa525334
Andreas Martin <andreas.martin@andreasmartin.ch>
2018-11-08 13:26:22 +0100

    Update to version 3.0.0

---
+++ src/main/java/ch/fhnw/digibp/ProcessApplication.java
@@ -0,0 +1,15 @@
+package ch.fhnw.digibp;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class ProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessApplication.class, args);
+	}
+
+}
